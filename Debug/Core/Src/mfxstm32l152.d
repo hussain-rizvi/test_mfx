854748d0056ca0056b8ab951b9975717ae956603
hussain-rizvi <hussain.rizvi1994@outlook.com>
2025-10-27 12:15:54 +0500

    removed debug

--- Debug/Core/Src/mfxstm32l152.d
+++
@@ -1,4 +0,0 @@
-Core/Src/mfxstm32l152.o: ../Core/Src/mfxstm32l152.c \
- ../Core/Inc/mfxstm32l152.h ../Core/Inc/mfxstm32l152_reg.h
-../Core/Inc/mfxstm32l152.h:
-../Core/Inc/mfxstm32l152_reg.h:
--- Debug/Core/Src/mfxstm32l152_reg.d
+++
@@ -1,3 +0,0 @@
-Core/Src/mfxstm32l152_reg.o: ../Core/Src/mfxstm32l152_reg.c \
- ../Core/Inc/mfxstm32l152_reg.h
-../Core/Inc/mfxstm32l152_reg.h:
